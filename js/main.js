f332aa114bfce820939f939412ba0d595ba67b2d
Jason Vanderslice <jlvanderslice@gmail.com>
2017-01-23 10:14:26 -0600

    Started from 7.3 lecture code and removed app specific code

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ajax } from 'jquery'
+import Validator from 'validator'
+
+export default React.createClass({
+  render() {
+    return(
+      <section>
+      </section>
+    )
+  }
+})
